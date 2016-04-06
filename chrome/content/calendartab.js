e10319449e2bf6c72889a7cfa5b26e74c172b769
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2016-04-06 17:41:36 +0200

    Initial version with a Calendar tab

---
+++ chrome/content/calendartab.js
@@ -0,0 +1,21 @@
+console.log('load');
+
+
+/*
+window.addEventListener("load", function(e) {
+	startup();
+}, false);
+
+window.setInterval(
+	function() {
+		startup();
+	}, 1000); //update date every minute
+
+function startup() {
+	var myPanel = document.getElementById("my-panel");
+	var date = new Date();
+	var day = date.getDay();
+	var dateString = date.getFullYear() + "." + (date.getMonth()+1) + "." + date.getDate();
+	myPanel.label = "Date: " + dateString;
+}
+*/
